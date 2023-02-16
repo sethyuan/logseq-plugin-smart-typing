542d8f0fd0518c13fb29e95846418a108f1d0185
Seth <sethyuan@gmail.com>
2023-02-16 11:14:03 +0800

    feat: add {{ handling and changed update text timing

--- src/typing.js
+++ src/typing.js
@@ -7,7 +7,7 @@ const TRIGGER_REGEX = 4
 
 const PairOpenChars = '([{"（【「『《〈“‘'
 const PairCloseChars = ')]}"）】」』》〉”’'
-const WrapIdenChars = "$\"'([¥￥（【「《·“‘”’"
+const WrapIdenChars = "$\"'([¥￥（【「《·“‘”’『"
 const WrapOpenChars = "$\"'([$$（【「《`“‘“‘"
 const WrapCloseChars = "$\"')]$$）】」》`”’”’"
 const BuiltInSpecialKeys = [
@@ -286,6 +286,14 @@ async function handlePairs(textarea, blockUUID, e) {
       return true
     }
     if (
+      (char === "『" || char === "「") &&
+      prevChar === "「" &&
+      nextChar === "」"
+    ) {
+      await updateText(textarea, blockUUID, `{{}}`, -2, 1, -2)
+      return true
+    }
+    if (
       nextChar == null ||
       PairCloseChars.includes(nextChar) ||
       Punc.test(nextChar)
@@ -315,7 +323,13 @@ async function handleSelection(textarea, blockUUID, e) {
       textarea.selectionStart,
       textarea.selectionEnd,
     )
-    if (prevChar === char && nextChar === WrapCloseChars[i]) {
+    if (
+      prevChar === "「" &&
+      nextChar === "」" &&
+      (char === "「" || char === "『")
+    ) {
+      await updateText(textarea, blockUUID, `{{${text}}}`, -1, 1, 0, 2)
+    } else if (prevChar === char && nextChar === WrapCloseChars[i]) {
       if (char === "（" || char === "(") {
         await updateText(textarea, blockUUID, `((${text}))`, -1, 1, 0, 2)
       } else if (char === "【" || char === "[") {
@@ -331,7 +345,7 @@ async function handleSelection(textarea, blockUUID, e) {
   }
 }
 
-function updateText(
+async function updateText(
   textarea,
   blockUUID,
   text,
@@ -345,33 +359,25 @@ function updateText(
   const endPos = textarea.selectionEnd + delEndOffset
   const newPos = startPos + text.length + cursorOffset
   const content = textarea.value
-  return new Promise((resolve, reject) => {
-    // HACK: postpone block update to guarantee other input events go first.
-    setTimeout(async () => {
-      try {
-        await logseq.Editor.updateBlock(
-          blockUUID,
-          startPos < content.length
-            ? `${content.substring(0, startPos)}${text}${content.substring(
-                endPos,
-              )}`
-            : startPos === content.length
-            ? `${content}${text}`
-            : `${content} ${text}`,
-        )
-      } catch (err) {
-        reject(err)
-      }
-      textarea.focus()
-      if (cursorOffset != null) {
-        textarea.setSelectionRange(
-          collapsed ? newPos : startPos + numWrapChars,
-          collapsed ? newPos : newPos - numWrapChars,
-        )
-      }
-      resolve()
-    }, 0)
-  })
+  try {
+    await logseq.Editor.updateBlock(
+      blockUUID,
+      startPos < content.length
+        ? `${content.substring(0, startPos)}${text}${content.substring(endPos)}`
+        : startPos === content.length
+        ? `${content}${text}`
+        : `${content} ${text}`,
+    )
+  } catch (err) {
+    reject(err)
+  }
+  textarea.focus()
+  if (cursorOffset != null) {
+    textarea.setSelectionRange(
+      collapsed ? newPos : startPos + numWrapChars,
+      collapsed ? newPos : newPos - numWrapChars,
+    )
+  }
 }
 
 function matchSpecialKey(text, start, trigger, skip = 0) {
